32d8195e8a68b577fc25a9fce64cab3432f35688
Andrew LaRiviere <andru.lariviere@gmail.com>
2019-08-09 13:30:09 -0700

    attempting to fix pre-processing

--- pricePredictor.py
+++ pricePredictor.py
@@ -21,40 +21,47 @@ def main():
     print("opening file...")
     inputFile = open("craigslistVehiclesFull.csv",encoding="utf8")
     print("reading data...")
-    importantAttributes = ["city","price","year","manufacturer","make","condition","cylinders","odometer","title_status","transmission"]
+    importantAttributes = ["city","price","year","manufacturer","make","condition","odometer","fuel","paint_color"]
     df = pd.read_csv(inputFile, usecols=importantAttributes) #read_csv, only selecting attributes with the most variance compared to price
     
     filtered = df[(df['city'] == 'losangeles')] #filter out losangeles data
-    filtered = filtered.sample(frac=1).reset_index(drop=True)  #shuffle dataset
-    filtered.dropna(inplace=True) #drop rows with empty columns
+    print(filtered.shape)
+    
+    #filtered = filtered.sample(frac=1).reset_index(drop=True)  #shuffle dataset
+    #filtered.dropna(inplace=True) #drop rows with empty columns
     
     #hard cast categorical columns for encoding
     filtered['manufacturer'] = filtered.manufacturer.astype(str)
     filtered['make'] = filtered.make.astype(str)
     filtered['condition'] = filtered.condition.astype(str)
-    filtered['cylinders'] = filtered.cylinders.astype(str)
-    filtered['transmission'] = filtered.transmission.astype(str)
+    filtered['paint_color'] = filtered.condition.astype(str)
+    filtered['fuel'] = filtered.condition.astype(str)
+    #filtered['cylinders'] = filtered.cylinders.astype(str)
+    #filtered['transmission'] = filtered.transmission.astype(str)
+    
+    Q1 = filtered.quantile(0.30)
+    Q3 = filtered.quantile(0.80)
+    IQR = Q3 - Q1
+    filteredMinusOutliers = filtered[~((filtered < (Q1 - 1.5 * IQR)) |(filtered > (Q3 + 1.5 * IQR))).any(axis=1)]
+    print(filteredMinusOutliers.shape)
     
     #encode categorical columns as integers
     print("Encoding values...")
-    for column in filtered.columns:
-        if filtered[column].dtype == type(object):
-            le = preprocessing.LabelEncoder()
-            filtered[column] = le.fit_transform(filtered[column])
+    #for column in filteredMinusOutliers.columns:
+    #    if filteredMinusOutliers[column].dtype == type(object):
+    #        le = preprocessing.LabelEncoder()
+    #        filteredMinusOutliers[column] = le.fit_transform(filteredMinusOutliers[column])
     
-    feature_cols = ["year","manufacturer","make","condition","cylinders","odometer","title_status","transmission"]
+    encoded = filteredMinusOutliers.apply(preprocessing.LabelEncoder().fit_transform)
     
-    X=filtered[feature_cols]
-    y=filtered.price
+    feature_cols = ["year","manufacturer","make","condition","fuel","odometer","paint_color"]
+    
+    X=encoded[feature_cols]
+    y=encoded.price
         
-    X_train, X_test, y_train, y_test = train_test_split( X, y, test_size = 0.20, random_state = 100) #split the dataset into training and test data
+    X_train, X_test, y_train, y_test = train_test_split( X, y, test_size = 0.30, random_state = 100) #split the dataset into training and test data
     
     dimitri = DecisionTreeClassifier() #classify a decision tree
-    
-    print("performing 10-fold cross validation...")
-    model = svm.SVC()
-    accuracy = cross_val_score(model, X, y, scoring='accuracy', cv = 10) #perform cross validation
-    print("Accuracy of Model with Cross Validation is:",accuracy.mean() * 100)
     
     print("training decision tree...")
     dimitri = dimitri.fit(X_train,y_train) #train it on the training data
@@ -62,6 +69,12 @@ def main():
     print("making price predictions...")
     y_pred = dimitri.predict(X_test) #use the decision tree to make predictions on test data
     print("Accuracy on test dataset is ", accuracy_score(y_test,y_pred)*100)
+    
+    print("performing 10-fold cross validation...")
+    model = svm.SVC()
+    accuracy = cross_val_score(model, X, y, scoring='accuracy', cv = 10) #perform cross validation
+    print("Accuracy of Model with Cross Validation is:",accuracy.mean() * 100)
+    
     
     inputFile.close()
     
